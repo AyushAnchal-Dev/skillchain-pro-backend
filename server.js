7968afc6c85d9418880b385802819e9d33c2759d
Ayush Anchal <abhardwaj8507@gmail.com>
2025-07-23 10:19:34 +0530

    Due to some errors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
-  credentials: true,
+  credentials: true
 }));
 
 app.use(express.json());
