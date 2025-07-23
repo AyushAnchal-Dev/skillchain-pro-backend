a4b886d839d3356a372cea5929b61e9743f62431
Ayush Anchal <abhardwaj8507@gmail.com>
2025-07-23 10:01:27 +0530

    For solving headers issue

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
+  credentials: true,
 }));
+router.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
